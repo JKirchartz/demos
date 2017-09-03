c2697567d90219b8c52e7be7af5e8e86c2bb6423
Joel Kirchartz <JKirchartz@gmail.com>
2017-09-03 12:28:24 -0400

    remove event listener
    CBDQ doesn't seem to trigger it

--- pipestrip.js
+++ pipestrip.js
@@ -1,20 +1,18 @@
 SVGElement.prototype.getTransformToElement = SVGElement.prototype.getTransformToElement || function(elem) {return elem.getScreenCTM().inverse().multiply(this.getScreenCTM());};
 var v = document.getElementById('viewport');
-v.addEventListener('load', function() {
-  var oH = 349, oW = 1200;
-  var nH = viewport.getAttribute('height'), nW = viewport.getAttribute('width');
-  var s = Math.round(Math.random()) ?
-      (Math.round(Math.random() * 10)) :
-      (Math.round(Math.random()*10) + ', ' + Math.round(Math.random()*10));
-  if(Math.round(Math.random() * 100) > 25) {
-    var st = document.getElementById('strip');
-    st.setAttribute('transform', 'scale(' + s + ')');
-    var m = st.getTransformToElement(v);
-    oW *= m.a;
-    oH *= m.d;
-  }
-  var cx = Math.floor(Math.random() * (oW - nW)), cy = Math.floor(Math.random() * (oH - nH));
-  v.setAttribute('width', nW);
-  v.setAttribute('height', nH);
-  v.setAttribute('viewBox', cx + ' ' + cy + ' ' + nW + ' ' + nH);
-});
+var oH = 349, oW = 1200;
+var nH = viewport.getAttribute('height'), nW = viewport.getAttribute('width');
+var s = Math.round(Math.random()) ?
+    (Math.round(Math.random() * 10)) :
+    (Math.round(Math.random()*10) + ', ' + Math.round(Math.random()*10));
+if(Math.round(Math.random() * 100) > 25) {
+  var st = document.getElementById('strip');
+  st.setAttribute('transform', 'scale(' + s + ')');
+  var m = st.getTransformToElement(v);
+  oW *= m.a;
+  oH *= m.d;
+}
+var cx = Math.floor(Math.random() * (oW - nW)), cy = Math.floor(Math.random() * (oH - nH));
+v.setAttribute('width', nW);
+v.setAttribute('height', nH);
+v.setAttribute('viewBox', cx + ' ' + cy + ' ' + nW + ' ' + nH);
